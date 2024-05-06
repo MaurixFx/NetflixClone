664c96d76a05250a4f7d663556d387e4e89b0be4
Mauricio Figueroa <maurixfx@gmail.com>
2024-05-06 10:33:12 +0200

    Initial Commit

---
+++ NetflixClone/NetflixCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  NetflixCloneApp.swift
+//  NetflixClone
+//
+//  Created by Mauricio Figueroa on 06-05-24.
+//
+
+import SwiftUI
+
+@main
+struct NetflixCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
